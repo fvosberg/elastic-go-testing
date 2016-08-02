16f343bff082dfe645100656982bd0b76d523b65
Frederik Vosberg <frederik.vosberg@diemedialen.de>
2016-08-02 08:18:10 +0200

    [BUGFIX] flush elastic search instead of wait for indexing to happen

--- src/registration_test.go
+++ src/registration_test.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/fvosberg/elastic-go-testing/customer"
 	"testing"
-	"time"
 )
 
 func TestRegistration(t *testing.T) {
 	testCustomer := customer.Customer{Email: "testing@test.de"}
 	testCustomer.Create()
-	time.Sleep(time.Second * 1)
+	client := customer.CustomerClient()
+	client.Flush()
 	_, err := customer.FindByEmail("testing@test.de")
 	if err != nil {
 		t.Logf("Error occured: %+v\n", err)
